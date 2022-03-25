0775cf6062a588be1be62ae8d22fcf7ea8f31bea
mahmood seoud <Mahmoodseoud@gmail.com>
2022-03-25 15:48:30 +0100

    Initial push

---
+++ RegisterAndLogin/RegisterAndLogin/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegisterAndLogin.Models;
+
+namespace RegisterAndLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            if (userModel.userName == "1234" && userModel.password == "1234")
+            {
+                return View("LoginSuccess", userModel);
+            }
+            else
+            {
+                return View("LoginFailure", userModel);
+            }
+        }
+    }
+}
---
+++ RegisterAndLogin/RegisterAndLogin/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿namespace RegisterAndLogin.Models
+{
+    public class UserModel
+    {
+        public int id { get; set; }
+        public string userName { get; set; }
+
+        public string password { get; set; }
+    }
+}
